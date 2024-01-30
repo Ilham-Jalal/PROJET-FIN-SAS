7184676dd25963d4bab376aa6ea5f64658ad5253
ilham-jalal <jalalilham55@gmail.com>
2024-01-30 16:55:28 +0100

    j'ajoute la fonction MENU dans la fonction principal main

--- BRIEF-FIN-SAS.c
+++ BRIEF-FIN-SAS.c
@@ -45,6 +45,53 @@ void Ajouter(){
 }
 
 int main() {
+	int main() {
+	
+    int choix;
+    
+    do {
+    	
+        printf("********MENU*******\n");
+        printf("1- Ajouter une tache\n");
+        printf("2- Afficher la liste des taches\n");
+        printf("3- Modifier une tache\n");
+        printf("4- Supprimer une tache\n");
+        printf("5- Ordonner une tache\n");
+        printf("6- Fitrer les taches\n");
+        printf("0- Quitter\n");
+        printf("Choisir une operation: ");
+        scanf("%d", &choix);
+
+        switch (choix) {
+        	
+            case 1:
+                Ajouter();
+                break;
+            case 2:
+                Afficher();
+                break;
+            case 3:
+                Modifier();
+                break;
+            case 4:
+                Supprimer();
+                break;
+            case 5:
+                OrdonnerParDate();
+                break;
+            case 6:
+                Filtrer();
+                break;
+            case 0:
+                printf("Au revoir !!\n");
+                break;
+            default:
+                printf("Choix invalide. Veuillez reessayer.\n");
+        }
+    } while (choix != 0);
+
+    return 0;
+}
 	
  
     return 0;
